24e6fb816a85d91dcc53344fcb332ab087de127e
YUCONG <yucong.ni@my.jcu.edu.au>
2022-12-05 21:28:46 +0800

    prac_4

--- prac_4/list_comprehensions.py
+++ prac_4/list_comprehensions.py
@@ -37,11 +37,17 @@ for i in full_names:
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: list comprehension to create a list of integers from the above list of strings
-# numbers =
+numbers = ([int(number) for number in almost_numbers])
+print(numbers)
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+greater_number = ([int(number) for number in numbers if number >= 9])
+print(greater_number)
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
+for name in full_names:
+    if len(name) > 11:
+        print(name[name.find(" ") + 1: len(name)], end=" ")
