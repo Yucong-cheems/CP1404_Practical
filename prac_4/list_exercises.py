9075bd7480926e0c00be7d12a41f765c13464f49
YUCONG <yucong.ni@my.jcu.edu.au>
2022-12-25 20:35:58 +0800

    prac 6 done

--- prac_4/list_exercises.py
+++ prac_4/list_exercises.py
@@ -11,11 +11,10 @@ for k in range(1):
     print(f"The largest number is {max(number)}")
     print(f"The average of the number is {sum(number) / 5}")
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 username = input("Enter your name: ")
 if username in usernames:
     print("Access granted")
 else:
     print("Access denied")
-
-
